ef1d2e34e8b23b50002613c9619c1144c645e551
Jon Wire <iambipedal@gmail.com>
2021-12-09 12:14:27 -0600

    2021 09 part 2

--- 2021/09/part1.c
+++ 2021/09/part1.c
@@ -17,6 +17,19 @@ Matrix new_matrix() {
 	return m;
 }
 
+Matrix new_empty_matrix(int width, int height) {
+	Matrix m = {
+		.width = width,
+		.height = height
+	};
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			m.data[y][x] = 0;
+		}
+	}
+	return m;
+}
+
 int max(int a, int b) {
 	return a >= b ? a : b;
 }
@@ -35,8 +48,6 @@ int to_int(char c) {
 }
 
 int risk_at(Matrix * m, int x, int y) {
-	// printf("assessing depth at %i, %i\n", x, y);
-
 	int target_score = 4;
 
 	if (x == 0 || x == m->width - 1) {
@@ -60,18 +71,14 @@ int risk_at(Matrix * m, int x, int y) {
 			if (_x == x || _y == y) {
 				int neighbor = m->data[_y][_x];
 				int self = m->data[y][x];
-				// printf("%i, %i (%i) > %i, %i (%i) => ", _x, _y, neighbor, x, y, self);
 				if (neighbor > self) {
-					// printf("true\n");
 					score++;
 				} else {
-					// printf("false\n");
 				}
 			}
 		}
 	}
 
-	// printf("x: %i, y: %i, score: %i, target: %i\n", x, y, score, target_score);
 	if (score == target_score) {
 		return m->data[y][x] + 1;
 	} else {
@@ -80,9 +87,7 @@ int risk_at(Matrix * m, int x, int y) {
 }
 
 Matrix risk_level(Matrix * m) {
-	Matrix risk = new_matrix();
-	risk.width = m->width;
-	risk.height = m->height;
+	Matrix risk = new_empty_matrix(m->width, m->height);
 
 	for (int y = 0; y < m->height; y++) {
 		for (int x = 0; x < m->width; x++) {
@@ -93,6 +98,44 @@ Matrix risk_level(Matrix * m) {
 	return risk;
 }
 
+int basin_score(Matrix * topography, Matrix * visits, int x, int y) {
+	int start_y = max(y - 1, 0);
+	int end_y = min(y + 1, topography->height - 1);
+
+	int start_x = max(x - 1, 0);
+	int end_x = min(x + 1, topography->width - 1);
+
+	int score = 1; // the center of the basin is itself worth a point
+
+	for (int _y = start_y; _y <= end_y; _y++) {
+		for (int _x = start_x; _x <= end_x; _x++) {
+			int neighbor = topography->data[_y][_x];
+			int self = topography->data[y][x];
+			if ((_x == x || _y == y) && neighbor != 9 && neighbor > self && visits->data[_y][_x] == 0) {
+				visits->data[_y][_x] = 1;
+				score += basin_score(topography, visits, _x, _y);
+			}
+		}
+	}
+
+	return score;
+}
+
+Matrix basin_scores(Matrix * topography, Matrix * risks) {
+	Matrix scores = new_empty_matrix(risks->width, risks->height);
+	Matrix visits = new_empty_matrix(risks->width, risks->height);
+
+	for (int y = 0; y < scores.height; y++) {
+		for (int x = 0; x < scores.width; x++) {
+			if (risks->data[y][x] > 0) {
+				scores.data[y][x] = basin_score(topography, &visits, x, y);
+			}
+		}
+	}
+	
+	return scores;
+}
+
 int matrix_sum(Matrix * m) {
 	int total = 0;
 	for (int y = 0; y < m->height; y++) {
@@ -101,6 +144,28 @@ int matrix_sum(Matrix * m) {
 		}
 	}
 	return total;
+}
+
+int by_int_reverse(const void * a, const void * b) {
+	int av = *(const int*)a;
+	int bv = *(const int*)b;
+	if (av < bv) return 1;
+	if (av > bv) return -1;
+	return 0;
+}
+
+int basin_threat(Matrix * m) {
+	int findings[m->width * m->height];
+
+	for (int y = 0; y < m->height; y++) {
+		for (int x = 0; x < m->width; x++) {
+			findings[y * m->width + x] = m->data[y][x];
+		}
+	}
+
+	qsort(&findings, m->width * m->height, sizeof(int), by_int_reverse);
+
+	return findings[0] * findings[1] * findings[2];
 }
 
 Matrix read_lines() {
@@ -145,5 +210,10 @@ int main() {
 	printf("risk levels\n");
 	print_matrix(&risk);
 
+	Matrix scores = basin_scores(&m, &risk);
+	printf("scores\n");
+	print_matrix(&scores);
+
 	printf("total risk: %i\n", matrix_sum(&risk));
+	printf("basin threat: %i\n", basin_threat(&scores));
 }
