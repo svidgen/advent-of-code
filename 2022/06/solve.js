6308100b410cc04aaffd05bdcf8f736c8d25c02d
Jon Wire <iambipedal@gmail.com>
2022-12-06 21:52:07 -0600

    2022 06 part 2

--- 2022/06/solve.js
+++ 2022/06/solve.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const data = fs.readFileSync(0, 'utf-8').trim();
 
-const MARKER_SIZE = 4;
+const PACKET_MARKER_SIZE = 4;
+const MESSAGE_MARKER_SIZE = 14;
 
 class Cursor {
 	data;
@@ -16,19 +17,23 @@ class Cursor {
 		this.position += count;
 	}
 
-	isAtMarker() {
+	isAtMarker(size) {
 		const seen = new Set();
-		for (let i = 0; i < MARKER_SIZE; i++) {
+		for (let i = 0; i < size; i++) {
 			seen.add(this.data[i + this.position]);
 		}
-		return seen.size === MARKER_SIZE;
+		return seen.size === size;
 	}
 }
 
 const cursor = new Cursor(data);
 
-while (!cursor.isAtMarker()) {
+while (!cursor.isAtMarker(PACKET_MARKER_SIZE)) {
 	cursor.advance();
 }
+console.log(cursor.position + PACKET_MARKER_SIZE);
 
-console.log(cursor.position + MARKER_SIZE);
+while (!cursor.isAtMarker(MESSAGE_MARKER_SIZE)) {
+	cursor.advance();
+}
+console.log(cursor.position + MESSAGE_MARKER_SIZE);
