d8ef02b49b824772d122da98ce61187ee18a5a2b
Jonathan Wire <iambipedal@gmail.com>
2022-12-08 21:45:04 -0600

    2022 08 part 1

---
+++ 2022/08/solve.js
@@ -0,0 +1,124 @@
+const sampleData = require('./sample');
+const realData = require('./data');
+
+class Grid {
+	data;
+	memos = new Map();
+
+	/**
+	 * Construct with data as text.
+	 * @param {String} data 
+	 */
+	constructor(lines) {
+		this.data = lines.trim().split('\n');
+	}
+
+	/**
+	 * @param {number} row 
+	 * @param {number} col 
+	 */
+	get(row, col) {
+		const memokey = `${row},${col}`;
+		if (!this.memos.has(memokey)) {
+			if (row >= 0 && row < this.data.length) {
+				const rowData = this.data[row];
+				if (col >= 0 && col < rowData.length) {
+					this.memos.set(
+						memokey,
+						new GridItem(this, row, col, Number(rowData[col]))
+					);
+				}
+			}
+		}
+		return this.memos.get(memokey);
+	}
+
+	/**
+	 * Width of the grid, assuming the first row is representative.
+	 */
+	get width() {
+		return this.data[0].length;
+	}
+
+	/**
+	 * Height of the grid.
+	 */
+	get height() {
+		return this.data.length;
+	}
+}
+
+class GridItem {
+	grid;
+	row;
+	col;
+	data;
+
+	maxHeights = {
+		north: undefined,
+		south: undefined,
+		east: undefined,
+		west: undefined,
+	};
+
+	constructor(grid, row, col, data) {
+		this.grid = grid;
+		this.row = row;
+		this.col = col;
+		this.data = data;
+	}
+
+	get north() {
+		return this.grid.get(this.row - 1, this.col);
+	}
+
+	get south() {
+		return this.grid.get(this.row + 1, this.col);
+	}
+
+	get east() {
+		return this.grid.get(this.row, this.col - 1);
+	}
+
+	get west() {
+		return this.grid.get(this.row, this.col + 1);
+	}
+
+	get isVisible() {
+		return this.isVisibleFrom('north') ||
+			this.isVisibleFrom('south') ||
+			this.isVisibleFrom('east') ||
+			this.isVisibleFrom('west')
+		;
+	}
+
+	maxHeightFrom(direction) {
+		if (this.maxHeights[direction] === undefined) {
+			this.maxHeights[direction] = Math.max(
+				this.data,
+				this[direction]?.maxHeightFrom(direction) || 0
+			);
+		}
+		return this.maxHeights[direction];
+	}
+
+	isVisibleFrom(direction) {
+		return this[direction] === undefined ||
+			this.data > this[direction]?.maxHeightFrom(direction)
+		;
+	}
+}
+
+let total = 0;
+const grid = new Grid(realData);
+console.log(grid)
+for (let row = 0; row < grid.height; row++) {
+	for (let col = 0; col < grid.width; col++) {
+		// console.log(`visiting ${row}, ${col} -> ${grid.get(row, col).data} -> ${grid.get(row, col).isVisible}`);
+		if (grid.get(row, col).isVisible) {
+			total += 1;
+		}
+	}
+}
+
+console.log(total);
