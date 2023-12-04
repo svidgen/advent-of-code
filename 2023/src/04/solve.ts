fa615dad25d247510cb1a99471f6b80a757fe26e
Jon Wire <iambipedal@gmail.com>
2023-12-04 17:04:27 -0600

    2023 day 4 part 2

--- 2023/src/04/solve.ts
+++ 2023/src/04/solve.ts
@@ -3,6 +3,9 @@ const raw = fs.readFileSync(0, 'utf-8');
 const lines = raw.split('\n') as string[];
 
 class Card {
+	// for part 2
+	public quantity: number = 1;
+
 	constructor(
 		public id: string,
 		public winners: number[],
@@ -18,24 +21,53 @@ class Card {
 		return new Card(id, winners, scratched);
 	}
 
-	get points() {
+	/**
+	 * The number of scratched numbers that are winners
+	 */
+	get hits() {
 		let hits = 0;
 		const winners = new Set(this.winners);
 		for (const scratched of this.scratched) {
 			if (winners.has(scratched)) hits++;
 		}
-		return hits > 0 ? Math.pow(2, hits - 1) : 0;
+		return hits;
+	}
+
+	// for part 1
+	get points() {
+		return this.hits > 0 ? Math.pow(2, this.hits - 1) : 0;
 	}
 }
 
 function part1() {
-	const cards = lines.filter(x => x).map(line => Card.parse(line));
-	for (const card of cards) {
-		console.log(`card ${card.id} : ${card.points}`);
-	}
 	return cards.reduce((sum, card) => sum + card.points, 0);
 }
 
+function part2() {
+	// assumes cards are in order.
+	for (const card of cards) {
+		console.log(`card ${card.id} : qty ${card.quantity}; hits ${card.hits};`);
+		for (let offset = 1; offset < card.hits + 1; offset++) {
+			const idToAward = (parseInt(card.id) + offset).toString();
+			const cardToAward = index.get(idToAward);
+			if (cardToAward) {
+				console.log(`awarding ${idToAward} : ${cardToAward.quantity} -> ${cardToAward.quantity + card.quantity}`);
+				cardToAward.quantity += card.quantity;
+			} else {
+				console.log(`no such card ${idToAward}`);
+			}
+		}
+	}
+	return cards.reduce((sum, card) => sum + card.quantity, 0);
+}
+
+const cards = lines.filter(x => x).map(line => Card.parse(line));
+const index = new Map<string, Card>();
+for (const card of cards) {
+	index.set(card.id, card);
+}
+
 console.log({
-	part1: part1()
+	part1: part1(),
+	part2: part2()
 });
