1f73a0a767d5516721224e1ab50b5a5b035f78bc
Jon Wire <iambipedal@gmail.com>
2023-12-14 21:05:15 -0600

    2023 day 14 part 2 ... sort of

--- 2023/src/14/solve.ts
+++ 2023/src/14/solve.ts
@@ -9,25 +9,6 @@ function sum(values: number[]): number {
 	return total;
 }
 
-function bitmap(str: string | string[], chr: string): number {
-	let b = 0;
-	for (const c of str) {
-		b = b << 1;
-		if (c === chr) b += 1;
-	}
-	return b;
-}
-
-function countBits(v: number): number {
-	let count = 0;
-	let n = v;
-	while (n) {
-		count += n & 1;
-		n = n >> 1;
-	}
-	return count;
-}
-
 type Coord = {
 	x: number;
 	y: number;
@@ -35,90 +16,168 @@ type Coord = {
 
 class Grid {
 	// [y][x]
-	data: string[] = [];
+	data: string[][] = [];
 	width: number = 0;
 	height: number = 0;
 	emptySpaces: { x: number[], y: number[] } | null = null;;
 
 	constructor(lines: string[]) {
-		this.data = lines;
+		this.data = lines.map(line => line.split(''));
 		this.height = this.data.length;
 		this.width = this.data[0]!.length;
-	}
-
-	static parse(lines: string[]): Grid[] {
-		const grids: Grid[] = [];
-		let _lines: string[] = [];
-
-		for (const line of lines) {
-			if (line.trim() === '') {
-				grids.push(new Grid(_lines));
-				_lines = [];
-			} else {
-				_lines.push(line);
-			}
-		}
-		
-		if (_lines.length > 0) grids.push(new Grid(_lines));
-		
-		return grids;
 	}
 
 	get(coord: Coord): string | undefined {
 		return this.data[coord.y]?.[coord.x];
 	}
 
-	vertical(x: number): string {
+	set(coord: Coord, value: string) {
+		// @ts-ignore
+		this.data[coord.y][coord.x] = value;
+	}
+
+	vertical(x: number): string[] {
 		let chars: string[] = [];
 		for (let y = 0; y < this.height; y++) {
 			chars.push(this.get({x, y})!);
 		}
-		return chars.join('');
+		return chars;
 	}
 
-	get verticals(): string[] {
-		const result: string[] = [];
+	setVertical(x: number, v: string[]) {
+		for (let y = 0; y < this.height; y++) {
+			this.set({x,y}, v[y]);
+		}
+	}
+
+	setHorizontal(y: number, h: string[]) {
+		for (let x = 0; x < this.width; x++) {
+			this.set({x,y}, h[x]);
+		}
+	}
+
+	horizontal(y: number): string[] {
+		let chars: string[] = [];
+		for (let x = 0; x < this.height; x++) {
+			chars.push(this.get({x, y})!);
+		}
+		return chars;
+	}
+
+	get verticals(): string[][] {
+		const result: string[][] = [];
 		for (let x = 0; x < this.width; x++) {
 			result.push(this.vertical(x));
 		}
 		return result;
 	}
+
+	set verticals(v: string[][]) {
+		for (let x = 0; x < this.width; x++) {
+			this.setVertical(x, v[x]);
+		}
+	}
+
+	get horizontals(): string[][] {
+		const result: string[][] = [];
+		for (let y = 0; y < this.height; y++) {
+			result.push(this.horizontal(y));
+		}
+		return result;
+	}
+
+	set horizontals(h: string[][]) {
+		for (let y = 0; y < this.height; y++) {
+			this.setHorizontal(y, h[y]);
+		}
+	}
+
+	shift(direction: string) {
+		switch (direction) {
+			case 'north':
+				this.verticals = this.verticals.map(v => shift(v));
+				break;
+			case 'south':
+				this.verticals = this.verticals.map(v => shift(v.reverse()).reverse());
+				break;
+			case 'west':
+				this.horizontals = this.horizontals.map(h => shift(h));
+				break;
+			case 'east':
+				this.horizontals = this.horizontals.map(h => shift(h.reverse()).reverse());
+				break;
+			default:
+				break;
+		}
+	}
+
+	rotate() {
+		this.shift('north');
+		this.shift('west');
+		this.shift('south');
+		this.shift('east');
+	}
+
+	get score() {
+		let points = 0;
+		for (let y = 0; y < this.height; y++) {
+			for (let x = 0; x < this.width; x++) {
+				const c = this.get({x, y});
+				if (c === 'O') {
+					points += this.height - y;
+				}
+			}
+		}
+		return points;
+	}
+
+	toString() {
+		return this.data.map(line => line.join('')).join('\n');
+	}
+
 }
 
-function score(line: string): number {
-	let points = 0;
+function shift(line: string[]): string[] {
+	let shifted: string[] = [];
 	let openIndex = 0;
 	for (let i = 0; i < line.length; i++) {
 		const c = line[i]!;
 		switch (c) {
 			case 'O':
-				// this is a rock that can slide up to the next open space.
-				// simulate its slide by awarding points and "consuming" the openIndex.
-				points += line.length - openIndex;
-
-				// the rock then "fills" the open spot.
+				shifted[i] = '.';
+				shifted[openIndex] = 'O';
 				openIndex += 1;
 				break;
 			case '#':
-				// no points awarded for this rock, which is fixed in place. instead of
-				// sliding up, it just sets the new open space, since any subsequent rocks
-				// cannot slide past it.
+				shifted[i] = '#';
 				openIndex = i + 1;
 				break;
 			default:
-				// nothing happens. we're in empty space.
+				shifted[i] = '.';
+				break;
 		}
 	}
-	return points;
+	return shifted;
 }
 
-const grid: Grid = new Grid(lines);
-
 const part1 = (() => {
-	return sum(grid.verticals.map(v => score(v)));
+	const grid: Grid = new Grid(lines);
+	grid.shift('north');
+	return grid.score;
 })();
 
 const part2 = (() => {
+	const grid: Grid = new Grid(lines);
+	const MAX_ITERS = 1000;
+	let i = 0;
+	let scores: number[] = [0,1];
+	while (scores.some(s => s !== scores[0]) && i < MAX_ITERS) {
+		grid.rotate();
+		scores.push(grid.score);
+		if (scores.length > 100) scores.shift();
+		i++;
+	}
+	return {scores: scores.map((v, idx) => `${idx}: ${v}`), i};
 })();
 
 console.log(JSON.stringify({part1, part2}, null, 2));
