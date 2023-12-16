097aa3fa03737ef6193078c6c7c87844c5a053b8
Jon Wire <iambipedal@gmail.com>
2023-12-16 12:35:48 -0600

    started pulling out commonalities

--- 2023/src/14/solve.ts
+++ 2023/src/14/solve.ts
@@ -1,95 +1,8 @@
-const fs = require('fs');
-const process = require('process');
-const raw = fs.readFileSync(process.argv[2], 'utf-8');
-const lines = raw.trim().split(/\n/);
+import { lines, Coord, Grid as BaseGrid } from '../common';
 
-function sum(values: number[]): number {
-	let total = 0;
-	for (const v of values) total+= v;
-	return total;
-}
-
-type Coord = {
-	x: number;
-	y: number;
-}
-
-class Grid {
-	// [y][x]
-	data: string[][] = [];
-	width: number = 0;
-	height: number = 0;
-	emptySpaces: { x: number[], y: number[] } | null = null;;
-
-	constructor(lines: string[]) {
-		this.data = lines.map(line => line.split(''));
-		this.height = this.data.length;
-		this.width = this.data[0]!.length;
-	}
-
-	get(coord: Coord): string | undefined {
-		return this.data[coord.y]?.[coord.x];
-	}
-
-	set(coord: Coord, value: string) {
-		// @ts-ignore
-		this.data[coord.y][coord.x] = value;
-	}
-
-	vertical(x: number): string[] {
-		let chars: string[] = [];
-		for (let y = 0; y < this.height; y++) {
-			chars.push(this.get({x, y})!);
-		}
-		return chars;
-	}
-
-	setVertical(x: number, v: string[]) {
-		for (let y = 0; y < this.height; y++) {
-			this.set({x,y}, v[y]);
-		}
-	}
-
-	setHorizontal(y: number, h: string[]) {
-		for (let x = 0; x < this.width; x++) {
-			this.set({x,y}, h[x]);
-		}
-	}
-
-	horizontal(y: number): string[] {
-		let chars: string[] = [];
-		for (let x = 0; x < this.height; x++) {
-			chars.push(this.get({x, y})!);
-		}
-		return chars;
-	}
-
-	get verticals(): string[][] {
-		const result: string[][] = [];
-		for (let x = 0; x < this.width; x++) {
-			result.push(this.vertical(x));
-		}
-		return result;
-	}
-
-	set verticals(v: string[][]) {
-		for (let x = 0; x < this.width; x++) {
-			this.setVertical(x, v[x]);
-		}
-	}
-
-	get horizontals(): string[][] {
-		const result: string[][] = [];
-		for (let y = 0; y < this.height; y++) {
-			result.push(this.horizontal(y));
-		}
-		return result;
-	}
-
-	set horizontals(h: string[][]) {
-		for (let y = 0; y < this.height; y++) {
-			this.setHorizontal(y, h[y]);
-		}
+class Grid extends BaseGrid<string> {
+	static parse(lines: string[]): Grid {
+		return new Grid(BaseGrid.parse(lines).data);
 	}
 
 	shift(direction: string) {
@@ -130,11 +43,6 @@ class Grid {
 		}
 		return points;
 	}
-
-	toString() {
-		return this.data.map(line => line.join('')).join('\n');
-	}
-
 }
 
 function shift(line: string[]): string[] {
@@ -161,13 +69,13 @@ function shift(line: string[]): string[] {
 }
 
 const part1 = (() => {
-	const grid: Grid = new Grid(lines);
+	const grid: Grid = Grid.parse(lines);
 	grid.shift('north');
 	return grid.score;
 })();
 
 const part2 = (() => {
-	const grid: Grid = new Grid(lines);
+	const grid: Grid = Grid.parse(lines);
 	const MAX_ITERS = 1000;
 	let i = 0;
 	let scores: number[] = [0,1];
