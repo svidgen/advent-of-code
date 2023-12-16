5a28f886900612eadbc71a9efc9cb4669d370919
Jon Wire <iambipedal@gmail.com>
2023-12-16 00:14:29 -0600

    2023 day 15 part 2

--- 2023/src/15/solve.ts
+++ 2023/src/15/solve.ts
@@ -3,12 +3,62 @@ const process = require('process');
 const raw = fs.readFileSync(process.argv[2], 'utf-8');
 const lines = raw.trim().split(/\n/);
 
+class HashMap {
+	buckets = [...Array(256)].map(b => new Bucket());
+
+	ingest(lens: Lens) {
+		const idx = hash(lens.label);
+		this.buckets[idx].ingest(lens);
+	}
+
+	checksum(): number {
+		let total = 0;
+		for (let i = 0; i < this.buckets.length; i++) {
+			total += this.buckets[i].checksum(i + 1);
+		}
+		return total;
+	}
+}
+
+class Bucket {
+	lenses = new Array<Lens>();
+
+	ingest(lens: Lens) {
+		const idx = this.lenses.findIndex(i => i.label === lens.label);
+		if (lens.op === '-') {
+			if (idx >= 0) this.lenses.splice(idx, 1);
+		} else if (lens.op === '=') {
+			if (idx >= 0) {
+				this.lenses[idx] = lens;
+			} else {
+				this.lenses.push(lens);
+			}
+		}
+	}
+
+	checksum(factor: number): number {
+		let total = 0;
+		for (let i = 0; i < this.lenses.length; i++) {
+			total += factor * (i + 1) * this.lenses[i].focalLength;
+		}
+		return total;
+	}
+}
+
+class Lens {
+	label: string;
+	op: string;
+	focalLength: number = 0;
+
+	constructor(s: string) {
+		const [_, label, op] = s.match(/^([a-z]+)(-|=\d+)$/)!;
+		this.label = label;
+		this.op = op === '-' ? '-' : '=';
+		if (this.op === '=') this.focalLength = parseInt(op.substring(1));
+	}
+}
+
 function hash(s: string): number {
-	// Algo from AOC 15:
-	// Determine the ASCII code for the current character of the string.
-	// Increase the current value by the ASCII code you just determined.
-	// Set the current value to itself multiplied by 17.
-	// Set the current value to the remainder of dividing itself by 256.
 	let h = 0;
 	for (const c of s) {
 		h += c.charCodeAt(0);
@@ -18,11 +68,14 @@ function hash(s: string): number {
 	return h;
 }
 
-// part 1
 let part1 = 0;
+const part2 = new HashMap();
 for (const line of lines) {
 	for (const part of line.split(',')) {
 		part1 += hash(part);
+		part2.ingest(new Lens(part));
 	}
 }
+
 console.log('part1', part1);
+console.log('part2', part2.checksum());
