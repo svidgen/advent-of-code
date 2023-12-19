4dc6948b44b42b715332203edba606eadb756698
Jon Wire <iambipedal@gmail.com>
2023-12-18 18:41:08 -0600

    2023 day 16 part 2

--- 2023/src/16/solve.ts
+++ 2023/src/16/solve.ts
@@ -60,7 +60,13 @@ class StepTracer<T> {
 		public grid: Grid<T>,
 		public cursorStep: CursorStep<T>
 	) {
+		this.reset();
+	}
+
+	reset() {
+		this.log = [];
 		this.state = Grid.fromDimensions(grid.width, grid.height, () => [] as string[]);
+		this.cursors = [];
 	}
 
 	add(cursor: Cursor) {
@@ -153,19 +159,56 @@ const tracer = new StepTracer(grid, (t, c) => {
 	c.step();
 });
 
+const visited = (t) => t.state.reduce((sum, cellState) => {
+	if (cellState.length > 0) sum++;
+	return sum;
+}, 0);
+
 (async () => {
 	tracer.add(new Cursor({x: 0, y: 0}, Direction.east));
-	await tracer.run();
+	tracer.run();
+	const part1 = visited(tracer);
 
-	const visited = tracer.state.reduce((sum, cellState) => {
-		if (cellState.length > 0) sum++;
-		return sum;
-	}, 0);
-
+	/*
 	console.log(tracer.grid.map((data, coord) => {
 		const state = tracer.state.get(coord)!;
 		return (data === '.' && state.length > 0) ? state[0] : data;
 	}).toString());
+	*/
 
-	console.log('part1', visited);
+	const width = tracer.grid.width;
+	const height = tracer.grid.height;
+
+	const origins = [
+		// west
+		...[...Array(height)].map(
+			(_, y) => new Cursor({x:0,  y}, Direction.east)
+		),
+		// north
+		...[...Array(width)].map(
+			(_, x) => new Cursor({x, y: 0}, Direction.south)
+		),
+		// east
+		...[...Array(height)].map(
+			(_, y) => new Cursor({x: width - 1, y}, Direction.west)
+		),
+		// south
+		...[...Array(width)].map(
+			(_, x) => new Cursor({x, y: height - 1}, Direction.north)
+		)
+	];
+
+	const scores = origins.map((cursor, i) => {
+		const origin = {...cursor, coord: {...cursor.coord}};
+		tracer.reset();
+		tracer.add(cursor);
+		tracer.run();
+		const v = visited(tracer);
+		console.log(`Processing ${i} (${JSON.stringify(origin)}) = ${v}`);
+		return v;
+	});
+
+	console.log('part 1', visited(tracer));
+	console.log('part 2', Math.max(...scores));
+
 })();
