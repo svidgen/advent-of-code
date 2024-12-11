3354ac1d89fd95f23a552794aed2a4548926d2f5
Jon Wire <iambipedal@gmail.com>
2024-12-10 19:49:00 -0600

    2024 day 10 part 1 and part 2

--- 2024/src/10/solve.ts
+++ 2024/src/10/solve.ts
@@ -2,34 +2,48 @@ import { lines, Grid, Coord } from '../common/index.js';
 
 const grid = Grid.parse(lines).map((v) => parseInt(v));
 
-function coordString(coord: Coord) {
+function key(coord: Coord) {
     return `${coord.x},${coord.y}`;
 }
 
 function search(
     grid: Grid<number>,
     from: Coord,
-    visited: Set<string> = new Set<string>()
+    unique: boolean,
+    visited: Set<string> = new Set<string>(),
 ) {
+    const k = key(from);
+
+    if (visited.has(k)) return [];
+    visited.add(k);
+
     const current = grid.get(from)!;
     if (current === 9) {
         return [from];
     }
-    visited.add(coordString(from));
 
     const results: Coord[] = [];
     for (const n of grid.neighbors(from, { withOrdinals: false })) {
-        if (visited.has(coordString(n))) continue;
         if (grid.get(n) === current + 1) {
-            results.push(...search(grid, n, visited));
+            results.push(...search(
+                grid, n, unique,
+                unique ? visited : new Set(...visited)
+            ));
         }
     }
     return results;
 }
 
-let count = 0;
-for (const head of grid.find(c => c === 0)) {
-    count += search(grid, head).length;
+
+function part(n: 1 | 2) {
+    let count = 0;
+    for (const head of grid.find(c => c === 0)) {
+        count += search(grid, head, n === 1 ? true : false).length;
+    }
+    return count;
 }
 
-console.log(count);
+
+
+console.log(part(1));
+console.log(part(2));
