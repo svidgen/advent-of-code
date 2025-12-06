618efb3db5a6c6731c8e2ff03f8d0f7b025fc726
Jon Wire <iambipedal@gmail.com>
2025-12-06 10:47:54 -0600

    indent using tabs

--- 2025/src/06/solve.ts
+++ 2025/src/06/solve.ts
@@ -3,24 +3,24 @@ import { lines, sum, transposeArray } from '../common/index.js';
 const data = transposeArray(lines.map(l => l.trim().split(/\s+/)));
 
 function part1() {
-    const equations = data.map(d => {
-        const operation = d.pop();
-        return {
-            operation,
-            operands: d.map(v => parseInt(v))
-        };
-    });
+	const equations = data.map(d => {
+		const operation = d.pop();
+		return {
+			operation,
+			operands: d.map(v => parseInt(v))
+		};
+	});
 
-    return sum(
-        equations.map(eq => {
-            let t = eq.operation === '*' ? 1 : 0;
-            for (const o of eq.operands) {
-                if (eq.operation === '*') t *= o;
-                if (eq.operation === '+') t += o;
-            }
-            return t;
-        })
-    );
+	return sum(
+		equations.map(eq => {
+			let t = eq.operation === '*' ? 1 : 0;
+			for (const o of eq.operands) {
+				if (eq.operation === '*') t *= o;
+				if (eq.operation === '+') t += o;
+			}
+			return t;
+		})
+	);
 }
 
 console.log('part 1', part1());
