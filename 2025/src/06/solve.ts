c8bc6be9125734c43128966c681e6358be8fa77b
Jon Wire <iambipedal@gmail.com>
2025-12-06 11:37:06 -0600

    2025 day 6 part 2

--- 2025/src/06/solve.ts
+++ 2025/src/06/solve.ts
@@ -1,26 +1,65 @@
-import { lines, sum, transposeArray } from '../common/index.js';
+import { Grid, lines, sum, transposeArray } from '../common/index.js';
 
-const data = transposeArray(lines.map(l => l.trim().split(/\s+/)));
+type Equation = {
+	operation: '+' | '*';
+	operands: number[];
+}
+
+function doMath(eq: Equation) {
+	let t = eq.operation === '*' ? 1 : 0;
+	for (const o of eq.operands) {
+		if (eq.operation === '*') t *= o;
+		if (eq.operation === '+') t += o;
+	}
+	return t;
+}
 
 function part1() {
+	const data = transposeArray(lines.map(l => l.trim().split(/\s+/)));
 	const equations = data.map(d => {
-		const operation = d.pop();
+		const operation = d.pop() as '+' | '*';
 		return {
 			operation,
 			operands: d.map(v => parseInt(v))
 		};
 	});
 
-	return sum(
-		equations.map(eq => {
-			let t = eq.operation === '*' ? 1 : 0;
-			for (const o of eq.operands) {
-				if (eq.operation === '*') t *= o;
-				if (eq.operation === '+') t += o;
+	return sum(equations.map(doMath));
+}
+
+function part2() {
+	const grid = Grid.parse(lines);
+	grid.defaultOnUndefined = () => ' ';
+	let total = 0;
+	let operation = '';
+	let i = 0;
+	let operandChars:  string[][] = [];
+	for (let x = grid.width - 1; x >= 0; x--) {
+		for (let y = 0; y < grid.height; y++) {
+			operandChars[y] = operandChars[y] || [];
+			const c = grid.get({ x, y });
+			if (c === '*' || c === '+') {
+				operation = c;
+			} else if (y !== grid.height - 1) {
+				operandChars[i].push(c || '');
 			}
-			return t;
-		})
-	);
+		}
+		i++;
+		if (operation === '*' || operation === '+') {
+			const operands = operandChars
+				.map(c => parseInt(c.join('')))
+				.filter(v => !Number.isNaN(v));
+			total += doMath({
+				operation,
+				operands
+			});
+			operandChars = [];
+			operation = '';
+			i = 0;
+		}
+	}
+	return total;
 }
 
 console.log('part 1', part1());
+console.log('part 2', part2());
