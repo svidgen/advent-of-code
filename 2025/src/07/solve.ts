c5f33740cd5f6bb15ec90f0d46e237a5bd00149a
Jon Wire <iambipedal@gmail.com>
2025-12-07 12:37:01 -0600

    2025 day 7 refactor

--- 2025/src/07/solve.ts
+++ 2025/src/07/solve.ts
@@ -1,46 +1,42 @@
 import { lines, sum } from '../common/index.js';
 
-function part1() {
+function solve() {
+	/**
+	 * Simple counter for the number of times a split occurs for part 1.
+	 */
 	let count = 0;
-	let state : boolean[] | undefined = undefined;
-	for (const line of lines) {
-		if (state === undefined) {
-			state = line.split('').map(c => c === 'S' ? true : false);
-			continue;
-		}
 
-		const splits = line.split('').map(c => c === '^' ? true : false);
-		for (let i = 0; i < splits.length; i++) {
-			if (splits[i] && state[i]) {
-				if (i > 0) state[i - 1] = true;
-				if (i < state.length - 1) state[i + 1] = true;
-				state[i] = false;
-				count++;
-			}
-		}
-	}
-	return count;
-}
-
-function part2() {
+	/**
+	 * State to count the number of beam "timelines" coming into each position
+	 * individually.
+	 * 
+	 * For part 1, this would only need to be a boolean to track whether there
+	 * *is* a beam. For part 2, we need to know specifically how many there are.
+	 */
 	let state : number[] | undefined = undefined;
+
 	for (const line of lines) {
 		if (state === undefined) {
 			state = line.split('').map(c => c === 'S' ? 1 : 0);
 			continue;
 		}
-
 		const splits = line.split('').map(c => c === '^' ? true : false);
 		for (let i = 0; i < splits.length; i++) {
 			if (splits[i] && state[i]) {
 				if (i > 0) state[i - 1] += state[i];
 				if (i < state.length - 1) state[i + 1] += state[i];
 				state[i] = 0;
+				count++; // part 1
 			}
 		}
 	}
-	return sum(state!);
+	return {
+		part1: count,
+		part2: sum(state!)
+	};
 }
 
-console.log('part 1', part1());
-console.log('part 2', part2());
+const { part1, part2 } = solve();
+
+console.log('part 1', part1);
+console.log('part 2', part2);
