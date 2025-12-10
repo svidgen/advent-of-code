57deef949b2f97669fb62aae6508fb34bd2c48a6
Jon Wire <iambipedal@gmail.com>
2025-12-10 12:00:47 -0600

    ...

--- 2025/src/10/solve.ts
+++ 2025/src/10/solve.ts
@@ -1,18 +1,18 @@
-import { lines, AutoPriorityQueue, sum } from '../common/index.js';
+import { lines, AutoPriorityQueue, sum, solveLinearSystem } from '../common/index.js';
 
 type BfsState<S> = { state: S, steps: number[], priority: number };
 
 type BfsOptions<S> = {
 	state: S;
 	visitedKey?: (state: S) => string;
+	hardFail?: (state: S) => boolean;
 	goal: (state: S) => boolean;
 	options: ((state: S) => S)[];
-	copyState?: ((state: S) => S);
 }
 
 function bfs<S>(bfsOptions: BfsOptions<S>): BfsState<S> | undefined {
-	const cp = (bfsOptions.copyState ? bfsOptions.copyState : s => JSON.parse(JSON.stringify(s))) as ((state: S) => S);
 	const keyof = (bfsOptions.visitedKey ? bfsOptions.visitedKey : s => JSON.stringify(s));
+	const hardFail = (bfsOptions.hardFail ? bfsOptions.hardFail : () => false);
 	const visited = new Set<string>();
 	const q = new AutoPriorityQueue<BfsState<S>>();
 
@@ -23,10 +23,11 @@ function bfs<S>(bfsOptions: BfsOptions<S>): BfsState<S> | undefined {
 
 		if (bfsOptions.goal(s.state)) return s;
 		if (visited.has(k)) continue;
+		if (hardFail(s.state)) continue;
 		visited.add(k);
 		
 		for (let i = 0; i < bfsOptions.options.length; i++) {
-			const newState = bfsOptions.options[i](cp(s.state));
+			const newState = bfsOptions.options[i](s.state);
 			q.enqueue({
 				state: newState,
 				steps: [...s.steps, i],
@@ -54,10 +55,7 @@ class Machine {
 		machine.target = line.match(/\[(.+)\]/)?.[1].split('').map(m => m === '#')!;
 		machine.lights = machine.target.map(() => false);
 		machine.buttons = line.match(/\([\d,]+\)/g)?.map(s => 
-			new Button(
-				s.substring(1, s.length - 1).split(',').map(v => parseInt(v)),
-				machine
-			)
+			new Button(s.substring(1, s.length - 1).split(',').map(v => parseInt(v)))
 		)!;
 		machine.joltages = line.match(/\{(.+)\}/)?.[1].split(',').map(v => parseInt(v))!;
 		return machine;
@@ -69,36 +67,42 @@ class Machine {
 		}
 	}
 
-	solve() {
+	solveLights() {
 		return bfs<boolean[]>({
 			state: [...this.lights],
 			goal: lights => lights.every((v, i) => v === this.target[i]),
-			options: this.buttons.map(b => (state: boolean[]) => b.press(state)),
+			options: this.buttons.map(b => (state: boolean[]) => b.toggleLights(state)),
 		})
+	}
+
+	solveJoltages() {
+		return solveLinearSystem([
+			// ...
+		])
 	}
 }
 
 class Button {
-	constructor(public wires: number[], public machine: Machine) {}
+	constructor(public wires: number[]) {}
 
-	/**
-	 * If `state` is given, the button simulates its push non-destructively against the
-	 * given `state` and returns a *new* state rather than manipulating the machine.
-	 * 
-	 * @param state 
-	 */
-	press(state?: boolean[]): boolean[] {
-		const s = state ? [...state] : this.machine.lights
+	toggleLights(state: boolean[]): boolean[] {
+		const s = [...state];
 		for (const wire of this.wires) s[wire] = !s[wire];
 		return s;
 	}
 }
 
+const machines = lines.map(line => Machine.parse(line));
+
 function part1() {
-	const machines = lines.map(line => Machine.parse(line));
-	const solutions = machines.map(m => m.solve()).map(s => s!.priority);
+	const solutions = machines.map(m => m.solveLights()).map(s => s!.priority);
 	return sum(solutions);
 }
 
+function part2() {
+	return '...'
+}
+
 console.log('part 1', part1());
+console.log('part 2', part2());
 
